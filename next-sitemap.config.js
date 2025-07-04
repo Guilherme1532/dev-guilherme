a0587c4d70a7ad939870adf1e0d5ca6b9be53c8c
Guilherme Ataide <guilhermeataide1532@gmail.com>
2025-07-04 04:52:22 -0300

    Alteração em projeto

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,17 +1,29 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://guilhermeaguiar.me',
+  siteUrl: "https://guilhermeaguiar.me",
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [
       {
-        userAgent: '*',
-        allow: '/',
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "*",
+        allow: "/_next/",
+      },
+      {
+        userAgent: "*",
+        allow: "/project-files/",
+      },
+      {
+        userAgent: "*",
+        allow: "/skills/",
       },
     ],
   },
-  changefreq: 'weekly',
+  changefreq: "weekly",
   priority: 0.7,
   sitemapSize: 5000,
-  exclude: ['/404', '/500'],
-}
+  exclude: ["/404", "/500"],
+};

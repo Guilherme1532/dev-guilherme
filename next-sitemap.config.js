4068cd09f7616e6dfb7972f1e6b0a202ec56eb1f
Guilherme Ataide <guilhermeataide1532@gmail.com>
2025-07-06 04:52:25 -0300

    Alteração na logo do projeto

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,13 +5,17 @@ module.exports = {
   changefreq: "weekly",
   priority: 0.7,
   sitemapSize: 5000,
-  exclude: ["/404", "/500"],
+  exclude: ["/404", "/500", "/projects/*"],
   additionalPaths: async (config) => {
     const projects = require("./src/data/projects.json");
     return [
-      { loc: "/", priority: 1.0, changefreq: "weekly" },
+      {
+        loc: "/",
+        changefreq: "daily",
+        priority: 1.0,
+      },
       ...projects.map((project) => ({
-        loc: `/projects/${project.id}`,
+        loc: `/projects/${project.name.toLowerCase().replace(/\s+/g, "-")}`,
         lastmod: new Date().toISOString(),
         changefreq: "weekly",
         priority: 0.8,
